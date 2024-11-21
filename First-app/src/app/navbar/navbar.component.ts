f9448831d7cfc6c0f94c95312e56bf0d108b308f
travail2 <lievinira8@gmail.com>
2024-11-21 14:00:25 +0200

    correctio

--- First-app/src/app/navbar/navbar.component.ts
+++ First-app/src/app/navbar/navbar.component.ts
@@ -3,10 +3,17 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    template: ` `
+    template: `<h1>bonjour vous tous</h1>
+               <p>Dummy Paragraph</p>
+               <button>Submit</button>`,
+
+    styles: `h1{
+            color:red;
+            background:black
+                }`,
+
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
 
-export class AppNavbar {
-
-
-}
+export class AppNavbar {}
