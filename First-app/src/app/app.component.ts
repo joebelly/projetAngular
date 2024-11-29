e4be7a4b0c17a213381e6abbd51970e22fd7ef16
travail2 <lievinira8@gmail.com>
2024-11-29 07:08:07 +0200

    commit

--- First-app/src/app/app.component.ts
+++ First-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, HeaderComponent],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,10 +20,28 @@ export class AppComponent {
 
   fruitName: string = 'Apple'
 
-  buttonClick(){
+  userName: string = 'John Doe';
+
+  textValue: string ='value is coming from component';
+
+  buttonClick() {
     console.log('Button Clicked');
   }
-  keyEnter(){
-    console.log('key pressed')
-  }
+  KeyEnter(event:any){
+    //console.log(event.keyCode);
+    if(event.keyCode == 72)
+    {
+      console.log('Enter key Pressed')
+    }
 }
+  keyupFiltering(user:HTMLInputElement) {
+    console.log(user.id);
+  }
+  updateUserName(username:HTMLInputElement) {
+
+  }
+
+  onKeyup() {
+    console.log(this.textValue);
+  }
+ }
